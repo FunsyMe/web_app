b23f47ac0c4b1592bc8ae05d6d299c7d3082c47f
FusnyMe <egorabramovich@mail.ru>
2025-11-11 20:13:35 +0300

    js and css

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
-document.addEventListener('mousemove', function (event) {
-    const centerX = window.innerWidth / 2;
-    const relativeX = event.clientX - centerX;
+const mainCard = document.querySelector('.main-card');
+
+let isMouseDown = false;
+let animationId = null;
+let currentMouseX = 0;
+
+function startTouch(event) {
+    isTouching = true;
+    currentTouchX = event.touches[0].clientX;
+    mainCard.style.boxShadow = '0 0 40px 4px #0080ce60'
     
-    console.log(relativeX);
-});
+    function animate() {
+        if (!isTouching) return;
+        
+        const centerX = window.innerWidth / 2;
+        const relativeX = currentTouchX - centerX;
+        
+        if (relativeX < -25) {
+            mainCard.style.transform = 'rotate(-4deg)';
+        }
+        else if (relativeX > 25) {
+            mainCard.style.transform = 'rotate(4deg)';
+        }
+        else {
+            mainCard.style.transform = 'rotate(0deg)';
+        }
+        
+        animationId = requestAnimationFrame(animate);
+    }
+    
+    animate();
+}
+
+function moveTouch(event) {
+    if (isTouching) {
+        currentTouchX = event.touches[0].clientX;
+    }
+}
+
+function stopTouch() {
+    isTouching = false;
+
+    mainCard.style.transform = 'rotate(0deg)';
+    mainCard.style.boxShadow = 'none'
+    
+    if (animationId) {
+        cancelAnimationFrame(animationId);
+    }
+}
+
+mainCard.addEventListener('touchstart', startTouch);
+mainCard.addEventListener('touchmove', moveTouch);
+mainCard.addEventListener('touchend', stopTouch);
+mainCard.addEventListener('touchcancel', stopTouch);
