ca117151145caeb3712a602a14240cc318aa8444
FusnyMe <egorabramovich@mail.ru>
2025-11-11 11:59:36 +0300

    js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-document.addEventListener('mousemove', function(event) {
-    const centerX = window.innerWeight / 2;
+document.addEventListener('mousemove', function (event) {
+    const centerX = window.innerWidth / 2;
     const relativeX = event.clientX - centerX;
-
-    console.log(`X: ${relativeX}`)
+    
+    console.log(relativeX);
 });
