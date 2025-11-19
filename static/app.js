581f4fb2d32c29a6838e8ea54cf56219dd0b935d
FusnyMe <egorabramovich@mail.ru>
2025-11-19 21:54:36 +0300

    html js css

--- static/app.js
+++ static/app.js
@@ -233,6 +233,13 @@ document.addEventListener('keydown', (e) => {
     }
 });
 
+const userAgent = navigator.userAgent.toLowerCase();
+const isMobile = /mobile|iphone|ipad|ipod|android|blackberry|mini|windows\\sce|palm/i.test(userAgent);
+
+if (!isMobile) {
+    window.location.href = './pc_index.html';
+}
+
 mainCard.addEventListener('mousedown', startMouse);
 mainCard.addEventListener('mousemove', moveMouse);
 mainCard.addEventListener('mouseup', stopMouse);
